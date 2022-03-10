ca137c4eed76a57371a0d6de33f9dc5b26b7c6be
Robert Anthony <rwanthony3@gmail.com>
2022-03-09 20:31:45 -0800

    current time added

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 var currentDayEl = $("#currentDay");
+var currentTimeEl = $("#currentTime");
+var el11am = $("#11am")
 
-function displayTime() {
+console.log(el11am);
+
+function displayDay() {
     var rightNow = moment().format("dddd MMM Do");
     currentDayEl.text(rightNow);
   }
 
+  function displayTime() {
+    var rightNow = moment().format("h:m a");
+    currentTimeEl.text(rightNow);
+  }
+
+displayDay();
 displayTime();
