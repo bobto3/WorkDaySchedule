0aa9d905446463cd4b74012130a5502e909aea51
Robert Anthony <rwanthony3@gmail.com>
2022-03-09 21:07:33 -0800

    background color test

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 var currentDayEl = $("#currentDay");
 var currentTimeEl = $("#currentTime");
-var el11am = $("#11am")
 
-console.log(el11am);
+//store element text content into variable
+var row9ALabelEl = $("#row9ALabel");
+var timeBlock9 = row9ALabelEl.text;
+var row9ABlockEl = $("#row9ABlock")
+
+row9ABlockEl.css("background-color", "red");
+console.log(row9ABlockEl);
+
+//compare text content to current time hour
+// function compareTime() {
+
+//     if (text content = current hour) {
+//         blockTextEl.css("background-color", "red")
+//     }
+//     if (text content < current hour) {
+//         blockTextEl.css("background-color", "gray")
+//     }
+//     if (text content > current hour) {
+//         blockTextEl.css("background-color", "green")
+//     }
+// }
 
 function displayDay() {
     var rightNow = moment().format("dddd MMM Do");
@@ -10,9 +29,10 @@ function displayDay() {
   }
 
   function displayTime() {
-    var rightNow = moment().format("h:m a");
+    var rightNow = moment().format("h:mm a");
     currentTimeEl.text(rightNow);
   }
 
+// compareTime();
 displayDay();
 displayTime();
